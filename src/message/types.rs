51d79b77cb6ad297d27cda1a591736eb8c542676
LinkTed <LinkTed@users.noreply.github.com>
2020-06-08 19:05:46 +0200

    Add PartialEq and Eq to MessageType

--- src/message/types.rs
+++ src/message/types.rs
@@ -1,7 +1,7 @@
 /// An enum representing the [message type].
 ///
 /// [message type]: https://dbus.freedesktop.org/doc/dbus-specification.html#message-protocol-types
-#[derive(Debug, Clone, FromPrimitive, ToPrimitive)]
+#[derive(Debug, PartialEq, Eq, Clone, FromPrimitive, ToPrimitive)]
 pub enum MessageType {
     MethodCall = 1,
     MethodReturn = 2,
