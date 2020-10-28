b3e0b3f1a58dcb30cd389407b0bc847d25e3cb3f
LinkTed <LinkTed@users.noreply.github.com>
2020-10-28 20:07:28 +0100

    Add cargo fuzz

---
+++ fuzz/fuzz_targets/decode_encode_decode.rs
@@ -0,0 +1,40 @@
+#![no_main]
+use bytes::BytesMut;
+use dbus_message_parser::{Decoder, Encoder};
+use libfuzzer_sys::fuzz_target;
+use std::cmp::Ordering;
+
+fuzz_target!(|data: &[u8]| {
+    let bytes = Vec::from(data);
+    let mut decoder = Decoder::new(&bytes);
+    if let Ok(msg_1) = decoder.message() {
+        let mut fds = Vec::new();
+        let mut bytes = BytesMut::default();
+        let mut encoder = Encoder::new(&mut bytes, &mut fds);
+        if let Err(e) = encoder.message(&msg_1) {
+            panic!("Could not encode DBus message: {:?}: {:?}", e, data);
+        }
+        let mut decoder = Decoder::new(&bytes);
+        match decoder.message() {
+            Ok(msg_2) => {
+                if let Some(ordering) = msg_1.partial_cmp(&msg_2) {
+                    match ordering {
+                        Ordering::Equal => {}
+                        _ => {
+                            panic!(
+                                "Messsage is not equal: {:?} != {:?}: {:?}",
+                                msg_1, msg_2, data
+                            );
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                panic!(
+                    "Could not decode DBus message: {:?}: {:?}: {:?}",
+                    e, bytes, data
+                );
+            }
+        }
+    }
+});
