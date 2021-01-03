92e51298f6ff52084ee26eecda9357acaa609053
LinkTed <link.ted@mailbox.org>
2021-01-03 18:52:23 +0100

    Remove Travis CI

--- .travis/zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-prefix="target/debug/deps"
-executables=("$prefix/dbus_message_parser-*.gc*"
-	     "$prefix/header-*.gc*"
-	     "$prefix/mod-*.gc*")
-
-zip -0 ccov.zip ${executables[@]}
