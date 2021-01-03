210bc0293481975a618667aad66e9955e30aa6e8
LinkTed <link.ted@mailbox.org>
2021-01-03 19:14:16 +0100

    Change fuzzer from AFL to Hunggfuzz

--- fuzz/src/decode_encode_decode.rs
+++ fuzz/src/decode_encode_decode.rs
@@ -1,42 +1,38 @@
 #[macro_use]
-extern crate afl;
-use bytes::BytesMut;
-use dbus_message_parser::{Decoder, Encoder};
+extern crate honggfuzz;
+use bytes::Bytes;
+use dbus_message_parser::message::Message;
 use std::cmp::Ordering;
 
 fn main() {
-    fuzz!(|data: &[u8]| {
-        let bytes = Vec::from(data);
-        let mut decoder = Decoder::new(&bytes);
-        if let Ok(msg_1) = decoder.message() {
-            let mut fds = Vec::new();
-            let mut bytes = BytesMut::default();
-            let mut encoder = Encoder::new(&mut bytes, &mut fds);
-            if let Err(e) = encoder.message(&msg_1) {
-                panic!("Could not encode DBus message: {:?}: {:?}", e, data);
-            }
-            let mut decoder = Decoder::new(&bytes);
-            match decoder.message() {
-                Ok(msg_2) => {
-                    if let Some(ordering) = msg_1.partial_cmp(&msg_2) {
-                        match ordering {
-                            Ordering::Equal => {}
-                            _ => {
-                                panic!(
-                                    "Messsage is not equal: {:?} != {:?}: {:?}",
-                                    msg_1, msg_2, data
-                                );
+    loop {
+        fuzz!(|data: &[u8]| {
+            let bytes = Bytes::copy_from_slice(data);
+            if let Ok((msg_1, _)) = Message::decode(bytes) {
+                let bytes = msg_1.encode().unwrap();
+                let bytes = bytes.freeze();
+                match Message::decode(bytes.clone()) {
+                    Ok((msg_2, _)) => {
+                        if let Some(ordering) = msg_1.partial_cmp(&msg_2) {
+                            match ordering {
+                                Ordering::Equal => {}
+                                _ => {
+                                    panic!(
+                                        "Messsage is not equal: {:?} != {:?}\n{:?} != {:?}",
+                                        msg_1, msg_2, data, bytes,
+                                    );
+                                }
                             }
                         }
                     }
-                }
-                Err(e) => {
-                    panic!(
-                        "Could not decode DBus message: {:?}: {:?}: {:?}",
-                        e, bytes, data
-                    );
+                    Err(e) => {
+                        panic!(
+                            "Could not decode DBus message:\n{:?}\n{:?}\n{:?}\n{:?}",
+                            e, bytes, data, msg_1
+                        );
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
